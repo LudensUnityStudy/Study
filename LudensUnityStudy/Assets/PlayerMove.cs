d905f14c78cb9e6267bed8a7403fec999ed10bdc
Song-Yerin <songyerin@khu.ac.kr>
2025-03-26 21:26:38 +0900

    Walking Anim 추가 버전

--- LudensUnityStudy/Assets/PlayerMove.cs
+++ LudensUnityStudy/Assets/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -7,6 +8,7 @@ public class PlayerMove : MonoBehaviour
 
     public float maxSpeed;
     public float jumpPower;
+    public bool Isjumping = false;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
@@ -23,15 +25,25 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !Isjumping)
         {
+            Isjumping = true;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             animator.SetBool("isJumping", true);
+            animator.SetBool("isWalking", false);
         }
             
 
         if (Input.GetButton("Horizontal"))
+        {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 0 && !Isjumping)
+        {
+            animator.SetBool("isWalking", false);
+        }
 
 
     }
@@ -45,15 +57,22 @@ public class PlayerMove : MonoBehaviour
 
         else if (rigid.velocity.x < (-1) * maxSpeed)
             rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
-        
+
+        // isWalking ¼³Á¤
+        if (!Isjumping)
+        {
+            animator.SetBool("isWalking", rigid.velocity.x > 0.1f || rigid.velocity.x < 0.1f);
+        }
+           
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Platform")
-        {        
-            animator.SetBool("isJumping", false);
-        }
+            
+        animator.SetBool("isJumping", false);
+        Isjumping = false;
+
     }
 }
 
